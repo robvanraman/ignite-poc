3a02b32e7e539e92330090049f31c054db2d5bb7
Raman Mishra <robvanraman@gmail.com>
2024-02-07 16:30:18 +0530

    Added ag grid add functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular';
+import { PositionsComponent } from './positions/positions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PositionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
